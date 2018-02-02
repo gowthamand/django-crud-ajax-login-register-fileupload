d9983abd94e3e26b632729ee7eedc24d95eb7efc
Gowthaman <gowthaman.nkl1@gmail.com>
2018-02-02 07:45:53 +0530

    changes

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,29 +1,30 @@
 """mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
 from django.contrib import admin
-from django.views.generic import TemplateView
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
 
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^polls/', include('polls.urls', namespace="polls")),
-    url(r'^', include('crud.urls')),
-    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
-    url(r'^login/$', auth_views.login, {'template_name': 'crud/login.html'}, name='login'),
-    url(r'^logout/$', auth_views.logout, {'template_name': 'crud/logged_out.html'}, name='logout'),
-]
+    path('admin/', admin.site.urls),
+    path('', include('crud.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('login/',auth_views.login,{'template_name':'login.html'},name='login'),
+    path('logout/',auth_views.logout,{'template_name':'logged_out.html'},name='logout'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
