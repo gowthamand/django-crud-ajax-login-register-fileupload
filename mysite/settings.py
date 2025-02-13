e33a483ecc8ff3440bc14cdce7a7723e5bd0ea5c
Gowthaman D <gowthaman.d@onetrust.com>
2025-02-13 15:37:13 +0530

    vercel set up

--- mysite/settings.py
+++ mysite/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
