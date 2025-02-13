2a840abf1abfbfdc1bc1c1f00ab2513636f2bd27
Gowthaman D <gowthaman.d@onetrust.com>
2025-02-13 14:59:54 +0530

    vercel set up

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,9 @@
-pip install -r requirements.txt
+which python3.9
+pwd
+cd /usr/local/bin/
+yum install sqlite-devel -y
+./configure --enable-optimizations --enable-loadable-sqlite-extensions
+make && make altinstall
+cd /vercel/path0/
+python3.9 install -r requirements.txt
 python3.9 manage.py collectstatic --noinput
