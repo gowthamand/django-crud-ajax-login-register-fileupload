af66fa5d4ffcd88341221e5b7d54dfcc0b1f0938
Gowthaman D <gowthaman.d@onetrust.com>
2025-02-13 15:05:20 +0530

    vercel set up

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,15 @@
-python3.9 install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+echo "BUILD START"
+
+# create a virtual environment named 'venv' if it doesn't already exist
+python3.9 -m venv venv
+
+# activate the virtual environment
+source venv/bin/activate
+
+# install all deps in the venv
+pip install -r requirements.txt
+
+# collect static files using the Python interpreter from venv
+python manage.py collectstatic --noinput
+
+echo "BUILD END"
